4674a827bccb9ffd4de97ad1374fedc996705b07
muhtarudinsiregar <6048482+muhtarudinsiregar@users.noreply.github.com>
2019-01-09 22:14:11 +0700

    Add php apache

---
+++ website/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php
+        $json = file_get_contents('http://product-service');
+        $obj = json_decode($json);
+
+        $products = $obj->products;
+        foreach ($products as $product) {
+            echo "<li>$product</li>";
+        }
+        ?>
+    </ul>
+</body>
+</html>
